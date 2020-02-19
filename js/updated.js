c68cb8448df65b107b01084ef23cb9e38d10b578
suyashsonawane <suyashsonawane007@gmail.com>
2020-02-19 14:48:55 +0530

    marquee and news

---
+++ js/updated.js
@@ -0,0 +1,36 @@
+ScrollRate = 50;
+
+function scrollDiv_init() {
+    DivElmnt = document.getElementById('newsDiv');
+    ReachedMaxScroll = false;
+
+    DivElmnt.scrollTop = 0;
+    PreviousScrollTop = 0;
+
+    ScrollInterval = setInterval('scrollDiv()', ScrollRate);
+}
+
+function scrollDiv() {
+
+    if (!ReachedMaxScroll) {
+        DivElmnt.scrollTop = PreviousScrollTop;
+        PreviousScrollTop++;
+
+        ReachedMaxScroll = DivElmnt.scrollTop >= (DivElmnt.scrollHeight - DivElmnt.offsetHeight);
+    } else {
+        ReachedMaxScroll = (DivElmnt.scrollTop == 0) ? false : true;
+
+        DivElmnt.scrollTop = PreviousScrollTop;
+        PreviousScrollTop--;
+    }
+}
+
+function pauseDiv() {
+    clearInterval(ScrollInterval);
+}
+
+function resumeDiv() {
+    PreviousScrollTop = DivElmnt.scrollTop;
+    ScrollInterval = setInterval('scrollDiv()', ScrollRate);
+}
+scrollDiv_init()
