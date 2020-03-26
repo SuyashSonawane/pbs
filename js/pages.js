a9619401aa2eed67532930b8152f9e8ad015a9e8
suyashsonawane <suyashsonawane007@gmail.com>
2020-03-26 11:53:23 +0530

    all done

--- js/pages.js
+++ js/pages.js
@@ -19,3 +19,10 @@ if (type && type < 6) {
     if (e == type - 1) return "active in";
   });
 } else $("#blog5").addClass("active in");
+
+if (type && type < 4) {
+  //   $(".fade")[type - 1].addClass("active");
+  $(".panel-collapse").addClass(e => {
+    if (e == type - 1) return "in";
+  });
+} else $("#collapse1").addClass(" in");
