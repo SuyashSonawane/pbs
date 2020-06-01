186706f8c4d51c70ffc01adc1120daf3b68af719
suyashsonawane <suyashsonawane007@gmail.com>
2020-06-01 18:32:58 +0530

    Mail updates 1

--- js/faculty.js
+++ js/faculty.js
@@ -1,7 +1,7 @@
 var type = window.location.hash.substr(1);
 // console.log(type);
-if (type && type < 5) {
-  $(".fal").addClass(e => {
+if (type && type < 8) {
+  $(".fal").addClass((e) => {
     if (e == type - 1) return "showFal";
   });
 }
