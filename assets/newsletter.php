c1b038c98e376f5e416e99e514c7758b7ddf3679
suyashsonawane <suyashsonawane007@gmail.com>
2020-10-08 11:01:30 +0530

    clear

--- assets/newsletter.php
+++
@@ -1,5 +0,0 @@
-<?php
-if($_POST["email_newsletter"]) {
-    mail("suyashsonawane@gmail.com", "Form to email message", $_POST["email_newsletter"], "From: an@email.address");
-}
-?>
--- js/faculty.js
+++
@@ -1,7 +0,0 @@
-var type = window.location.hash.substr(1);
-// console.log(type);
-if (type && type < 8) {
-  $(".fal").addClass((e) => {
-    if (e == type - 1) return "showFal";
-  });
-}
